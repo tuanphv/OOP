e2e0d5ee01b878f932d5ea8b3f0f18131e2e5cba
Nhan <thanhnhan140705@gmail.com>
2024-11-16 14:56:59 +0700

    add pn

---
+++ src/PhieuNhap.java
@@ -0,0 +1,12 @@
+public class PhieuNhap {
+    private String maPhieuNhap;
+    private String ngayNhap;
+    private String maNhanVien;
+    private String maNhaCCap;
+    private int tongTien;
+
+    public PhieuNhap() {}
+    public PhieuNhap(String maPhieuNhap, String ngayNhap, String maNhanVien){
+
+    }
+}
