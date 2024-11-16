707d04377d53f7d68e57eafbe23b2ab4717a07ae
tuanphv <tuanpv2604@gmail.com>
2024-11-16 20:33:58 +0700

    [ADD] Quản lý sách

--- src/QuanLySach.java
+++ src/QuanLySach.java
@@ -1,15 +1,99 @@
+import java.util.Scanner;
+
 public class QuanLySach {
     DSSach dsSach = new DSSach();
 
     public void Menu() {
-        System.out.println("1. Them sach");
-        System.out.println("2. Xoa sach");
-        System.out.println("3. Tim sach theo ma sach");
-        System.out.println("4. Tim sach theo ma tac gia");
-        System.out.println("5. Tim sach theo nam xuat ban");
-        System.out.println("6. Tim sach theo the loai");
-        System.out.println("7. Tim sach theo don gia");
-        System.out.println("8. Hien thi danh sach sach");
-        System.out.println("9. Thoat");
+        Scanner in = new Scanner(System.in);
+        int chon = 0;
+        System.out.println("Quan ly sach");
+        do {
+            System.out.println("1. Them sach");
+            System.out.println("2. Xoa sach");
+            System.err.println("3. Tim sach");
+            System.out.println("4. Hien thi danh sach sach");
+            System.out.println("5. Thoat");
+            System.out.println("Chon: ");
+            chon = Integer.parseInt(in.nextLine());
+            switch (chon) {
+                case 1:
+                    int chonLoai = 0;
+                    do {
+                        System.out.println("Chon loai sach: 1. Sach hoc thuat, 2. Sach giai tri");
+                        chonLoai = Integer.parseInt(in.nextLine());
+                        switch (chonLoai) {
+                            case 1:
+                                SachHocThuat sht = new SachHocThuat();
+                                dsSach.add(sht);
+                                break;
+                            case 2:
+                                SachGiaiTri sgt = new SachGiaiTri();
+                                dsSach.add(sgt);
+                                break;
+                            default:
+                                System.out.println("Chon sai! Vui long chon lai");
+                                break;
+                        }
+                    } while (chonLoai != 1 && chonLoai != 2);
+                    break;
+                case 2:
+                    System.out.println("Nhap ma sach can xoa: ");
+                    String maSachXoa = in.nextLine();
+                    dsSach.remove(dsSach.get(maSachXoa));
+                    break;
+                case 3:
+                    int chonTim = 0;
+                    do {
+                        System.out.println("Chon cach tim kiem");
+                        System.out.println("1. Tim theo ten sach");
+                        System.out.println("2. Tim theo ma sach");
+                        System.out.println("3. Tim theo nam xuat ban");
+                        System.out.println("4. Tim theo khoang gia");
+                        System.out.println("5. Thoat");
+                        System.out.println("Chon: ");
+                        chonTim = Integer.parseInt(in.nextLine());
+                        switch (chonTim) {
+                            case 1:
+                                System.out.println("Nhap ten sach can tim: ");
+                                String tenSach = in.nextLine();
+                                dsSach.timTheoTenSach(tenSach);
+                                break;
+                            case 2:
+                                System.out.println("Nhap ma sach can tim: ");
+                                String maSachTim = in.nextLine();
+                                dsSach.get(maSachTim);
+                                break;
+                            case 3:
+                                System.out.println("Nhap nam xuat ban can tim: ");
+                                int namXB = Integer.parseInt(in.nextLine());
+                                dsSach.timTheoNamXB(namXB);
+                                break;
+                            case 4:
+                                System.out.println("Nhap khoang gia can tim: ");
+                                int giaMin = Integer.parseInt(in.nextLine());
+                                int giaMax = Integer.parseInt(in.nextLine());
+                                dsSach.timTheoDonGia(giaMin, giaMax);
+                                break;
+                            case 5:
+                                System.out.println("Ket thuc tim kiem");
+                                break;
+                            default:
+                                System.out.println("Chon sai! Vui long chon lai");
+                                break;
+                        }
+                    } while (chonTim!=5);
+                    break;
+                case 4:
+                    dsSach.xuat();
+                    break;
+                case 5:
+                    System.out.println("Ket thuc");
+                    break;
+                default:
+                    System.out.println("Chon sai! Vui long chon lai");
+                    break;
+            }
+        } while (chon == 5);
+        in.close();
     }
 }
